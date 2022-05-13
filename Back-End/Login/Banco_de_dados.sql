73aea691488841ccd368683f9c434f29d0e63c3c
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-05-13 20:20:31 -0300

    Criação de banco de dados no MySQL Workbench

---
+++ Back-End/Login/Banco_de_dados.sql
@@ -0,0 +1,9 @@
+create database marketplace;
+use marketplace;
+create table logins (
+USER_ID int(4) auto_increment primary key,
+Email varchar(40) not null unique,
+Senha varchar(30) not null
+);
+describe logins;
+select * from logins
