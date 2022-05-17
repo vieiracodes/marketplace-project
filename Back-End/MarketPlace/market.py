d207dcc6d70a7060c3431281c5f19c990815f075
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-05-16 23:59:20 -0300

    Adicão inicial dos arquivos para as outras funcionalidades do site

---
+++ Back-End/MarketPlace/market.py
@@ -0,0 +1,27 @@
+#Gerenciar produtos
+
+#O python atuará como um programa serverside
+from flask import Flask, request
+from flask import redirect, url_for, render_template #Manipulação de URL e HTML
+import mysql.connector #comunicação com o banco de dados
+from mysql.connector import errors
+
+#Criação de uma instância do Flask
+app = Flask(__name__)
+
+#Conexão com o banco de dados
+comms = mysql.connector.connect(
+  host="localhost",
+  user="Py_auth",
+  password="Py_auth",
+  database="marketplace"
+)
+cursor = comms.cursor()
+
+
+#CÓDIGO
+
+#Processo para que o aplicativo seja exportado e executado ao clicar no botão HTMl
+
+if (__name__ == '__main__'):
+    app.run(debug= True)
