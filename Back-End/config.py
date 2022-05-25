5ee6806e5384d5ff0dedf294064b5766565d2bdb
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-05-25 19:04:02 -0300

    Beta do arquivo que armazenará alguns parâmetros usados no back-end

---
+++ Back-End/config.py
@@ -0,0 +1,8 @@
+secret_key = 'insert_secret_key'
+
+#Banco de dados - Teste local
+platform="PostgreSQL_local"
+host="localhost"
+user="Inser_User" #Inserir o usuário de acesso ao seu banco de dados
+password="Insert_Password" #Inserir a senha de acesso ao seu banco de dados
+database="marketplace" #Inserir o banco de dados. Aqui, foi nomeado de marketplace
