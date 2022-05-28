887fe4409e9dd5fb910cb4cc10a62cda1d3f2512
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-05-28 18:10:57 -0300

    Adequação do arquivo Banco_de_dados.sql, para torná-lo mais didático

--- Back-End/Login/Banco_de_dados.sql
+++ Back-End/Login/Banco_de_dados.sql
@@ -1,9 +1,40 @@
-create database marketplace;
-use marketplace;
-create table logins (
-USER_ID int(4) auto_increment primary key,
+-- Comandos para a criação do banco de dados
+
+--(===============================<para MySQL>===============================)--
+
+-- create database marketplace;
+-- use marketplace;
+
+---------------------------<Tabela de Logins>---------------------------
+
+-- create table logins (
+-- id int(4) auto_increment primary key,
+-- Email varchar(40) not null unique,
+-- Senha varchar(88) not null
+-- );
+-- OBS: O campo de senha está com 88 caracteres pois ela é criptografada
+
+---------------------------<Visualizar Dados>---------------------------
+-- Descomentar abaixo
+-- describe logins;
+-- select * from logins;
+
+--(=============================<para PostgreSQL>============================)--
+
+-- Criar database pelo pgadmin sem instruções
+-- As instruções estão bugadas
+
+-- create database IF NOT EXISTS marketplace;
+-- select testedb;
+
+---------------------------<Tabela de Logins>---------------------------
+create table if not exists logins (
+id serial,
 Email varchar(40) not null unique,
-Senha varchar(30) not null
+Senha varchar(88) not null
 );
-describe logins;
-select * from logins;
+-- OBS: O campo de senha está com 88 caracteres pois ela é criptografada
+
+---------------------------<Visualizar Dados>---------------------------
+-- describe logins;
+-- select * from logins;
