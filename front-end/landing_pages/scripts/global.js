3c8c69883fb7e4ca2c799e06cce5ec34a2d4a9b2
Gabriel Vieira <vieira.front-end@outlook.com>
2022-06-10 11:06:56 -0300

    update

--- front-end/landing_pages/scripts/global.js
+++ front-end/landing_pages/scripts/global.js
@@ -1,5 +1,5 @@
 const menuButton = document.querySelector('ul .menu-icon')
-let c = 2
+let b = 2
 
 menuButton.addEventListener('touchstart', changeBtns)
 menuButton.addEventListener('click', changeBtns)
@@ -12,17 +12,17 @@ if (window.innerWidth > 768){
 
 function changeBtns(){
     let btnsContainer = document.querySelector('.btns')
-    if(c%2 == 0){
+    if(b%2 == 0){
         btnsContainer.style.display = 'flex'
         setTimeout(()=>{
             btnsContainer.style.opacity = '1'
         }, 200)
-        c++
+        b++
     } else{
         btnsContainer.style.opacity = '0'
         setTimeout(()=>{
             btnsContainer.style.display = ''
         },200)
-        c++
+        b++
     }
 }
